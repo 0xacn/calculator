d02ce3798c5c25ba1c4cbf13bf4d5657b6a7c4c3
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2020-11-21 10:07:48 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,17 @@
 class Calculator:
-  
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    
+    def divide(self.x, self.y):
+      self.x // self.y
+     
+    
+    def multiply(self.x, self.y):
+      self.x * self.y
+      
+    def addition(self.x, self.y):
+      self.x + self.y
+     
+    def subtraction(self.x, self.y):
+      self.x - self.y
